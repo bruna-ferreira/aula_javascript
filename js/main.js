fcbab2d7adcce23c726af3427c91675a10ae4d21
bruna-ferreira <bruna_m_ferreira@outlook.com>
2020-11-21 20:56:27 -0300

    Atualizações

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+var nome = "Bruna Marchioni";
+var n1 = 30;
+var n2 = 10;
+var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos ");
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toLowerCase());
+//alert(frase.replace("Japão", "Brasil"));
+
